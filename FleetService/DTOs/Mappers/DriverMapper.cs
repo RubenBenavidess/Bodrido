8374ab61c0716d5699db9c25814c705258b3f9d2
dr00p3r <joancobe64@gmail.com>
2025-12-16 19:08:55 -0500

    feat: Mappers para los DTOs

---
+++ FleetService/DTOs/Mappers/DriverMapper.cs
@@ -0,0 +1,32 @@
+﻿using FleetService.DTOs.Request;
+using FleetService.DTOs.Response;
+using FleetService.Models;
+
+namespace FleetService.DTOs.Mappers
+{
+    public class DriverMapper
+    {
+        public static Driver ToEntity(DriverRequestDto dto)
+        {
+            return new Driver
+            {
+                UserId = dto.UserId,
+                LicenseNumber = dto.LicenseNumber,
+                LicenseCategory = dto.LicenseCategory,
+            };
+        }
+
+        public static DriverResponseDto ToDto(Driver entity)
+        {
+            return new DriverResponseDto
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                LicenseNumber = entity.LicenseNumber,
+                Status = entity.Status,
+                CurrentVehicleId = entity.CurrentVehicleId,
+                CurrentVehiclePlate = entity.CurrentVehicle?.Plate
+            };
+        }
+    }
+}

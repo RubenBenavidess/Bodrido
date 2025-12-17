457ded9a8e55f3cec8317e44c593cfa8834967ab
dr00p3r <joancobe64@gmail.com>
2025-12-16 19:11:05 -0500

    feat: Servicio de Vehiculos

---
+++ FleetService/Services/IVehicleService.cs
@@ -0,0 +1,12 @@
+﻿using FleetService.DTOs.Request;
+using FleetService.DTOs.Response;
+
+namespace FleetService.Services
+{
+    public interface IVehicleService
+    {
+        Task<VehicleResponseDto> CreateVehicleAsync(VehicleRequestDto requestDto);
+        Task<VehicleResponseDto?> GetVehicleByIdAsync(Guid id);
+        Task<IEnumerable<VehicleResponseDto>> GetAllVehiclesAsync();
+    }
+}
---
+++ FleetService/Services/Implementations/VehicleService.cs
@@ -0,0 +1,37 @@
+﻿using FleetService.Data;
+using FleetService.DTOs.Mappers;
+using FleetService.DTOs.Request;
+using FleetService.DTOs.Response;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetService.Services.Implementations
+{
+    public class VehicleService(FleetContext _context) : IVehicleService
+    {
+        public async Task<VehicleResponseDto> CreateVehicleAsync(VehicleRequestDto requestDto)
+        {
+            var vehicle = VehicleMapper.toEntity(requestDto);
+
+            _context.Vehicles.Add(vehicle);
+
+            await _context.SaveChangesAsync();
+
+            return VehicleMapper.toDto(vehicle);
+        }
+
+        public async Task<VehicleResponseDto?> GetVehicleByIdAsync(Guid id)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(id);
+            return vehicle is null ? null : VehicleMapper.toDto(vehicle);
+        }
+
+        public async Task<IEnumerable<VehicleResponseDto>> GetAllVehiclesAsync()
+        {
+            var vehicles = await _context.Vehicles
+                                            .AsNoTracking()
+                                            .ToListAsync();
+
+            return vehicles.Select(VehicleMapper.toDto);
+        }
+    }
+}

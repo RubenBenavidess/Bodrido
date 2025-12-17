58be716cf6a324c116f01fc0f01f3e60b9683ba9
dr00p3r <joancobe64@gmail.com>
2025-12-17 00:32:55 -0500

    feat: updated seed

--- src/scripts/seed.js
+++ src/scripts/seed.js
@@ -15,9 +15,12 @@ async function seed() {
 
         // 1. Crear Permisos (Scopes)
         const p1 = await Permission.create({ slug: "order:create", description: "Crear pedidos" });
-        const p2 = await Permission.create({ slug: "order:view", description: "Ver pedidos" });
-        const p3 = await Permission.create({ slug: "report:view", description: "Ver reportes" });
-        const p4 = await Permission.create({ slug: "fleet:update", description: "Actualizar flota" });
+        const p2 = await Permission.create({ slug: "order:update", description: "Actualizar pedidos" });
+        const p3 = await Permission.create({ slug: "order:view", description: "Ver pedidos" });
+        const p4 = await Permission.create({ slug: "order:view_nopicked", description: "Ver pedidos no recogidos" });
+        const p5 = await Permission.create({ slug: "fleet:create", description: "Crear flota" });
+        const p6 = await Permission.create({ slug: "fleet:update", description: "Actualizar flota" });
+        const p7 = await Permission.create({ slug: "fleet:view", description: "Ver flota" });
 
         // 2. Crear Roles
         const roleAdmin = await Role.create({ name: "ADMIN" });
@@ -26,9 +29,10 @@ async function seed() {
         const roleSuper = await Role.create({ name: "SUPERVISOR" });
 
         // 3. Asignar Permisos a Roles (Magia de Sequelize)
-        await roleAdmin.addPermissions([p1, p2, p3, p4]); // Admin hace todo
-        await roleDriver.addPermissions([p2, p4]);       // Driver ve pedidos y actualiza flota
-        await roleClient.addPermissions([p1, p2]);       // Cliente crea y ve pedidos
+        await roleAdmin.addPermissions([p1, p2, p3, p4, p5, p6, p7]); // Admin hace todo
+        await roleSuper.addPermissions([p2, p3, p5, p6]);      // Supervisor ve y actualiza pedidos y flota
+        await roleDriver.addPermissions([p4]);              // Driver ve pedidoss no recogidos
+        await roleClient.addPermissions([p1, p3]);       // Cliente crea y ve pedidos
 
         // 4. Crear Zonas
         await Zone.create({ nombre_ciudad: "Quito", zona_geo: "NORTE" });

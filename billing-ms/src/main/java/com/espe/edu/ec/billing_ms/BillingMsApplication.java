5033501cddf74f89c6061164e291e61722e0b7d3
RubenBenavidess <dario.benavides@hotmail.com>
2025-12-16 22:49:17 -0500

    chore(billing-ms): created billing ms project init

---
+++ billing-ms/src/main/java/com/espe/edu/ec/billing_ms/BillingMsApplication.java
@@ -0,0 +1,13 @@
+package com.espe.edu.ec.billing_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingMsApplication.class, args);
+	}
+
+}
---
+++ billing-ms/src/test/java/com/espe/edu/ec/billing_ms/BillingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.edu.ec.billing_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

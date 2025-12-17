e5f404f516434c9508205c3ceba0b83c4c8e45bd
RubenBenavidess <dario.benavides@hotmail.com>
2025-12-16 22:27:16 -0500

    chore(routing-ms): created routing-ms init project

---
+++ routing-ms/src/main/java/com/espe/edu/ec/routing_ms/RoutingMsApplication.java
@@ -0,0 +1,13 @@
+package com.espe.edu.ec.routing_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingMsApplication.class, args);
+	}
+
+}
---
+++ routing-ms/src/test/java/com/espe/edu/ec/routing_ms/RoutingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.edu.ec.routing_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
